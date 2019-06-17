eb4fa1a697cd0b466eda35af027ad575a2d7b50e
python060319 <51337287+python060319@users.noreply.github.com>
2019-06-17 20:02:06 +0300

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,20 @@
+x = int(input("Please enter a number:"))
+if x > 0:
+    # do this if x is bigger than 0
+    print(f'{x} is bigger than zero')
+    print('this is all i want to do if > 0');
+elif x == 0:
+    print (f'{x} is zero')
+else:
+    print(f'{x} is smaller than zero')
+
+print('back to normal........')
+
+
+# read string from user
+# check if the letter 'a' appears
+# if so, print how many times 'a' appears
+# elif check if letter 'b' appears
+# if so, print how many times 'b' appears
+# else print 'no!'
+
